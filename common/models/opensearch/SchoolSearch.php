e4871759edf56a6b9376fa3283587761a335ba6e
HowieBird <76828528@qq.com>
2017-10-05 15:36:17 +0800

    机构搜索

---
+++ common/models/opensearch/SchoolSearch.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 2017/10/5
+ * Time: 下午3:35
+ */
+
+namespace common\models\opensearch;
+
+
+class SchoolSearch extends SearchOS
+{
+    public static function createSearch()
+    {
+
+    }
+
+    public static function deleteSearch()
+    {
+
+    }
+
+    public static function updateSearch()
+    {
+
+    }
+
+    public static function search()
+    {
+        
+    }
+}
