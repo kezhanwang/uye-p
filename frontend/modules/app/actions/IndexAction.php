0c9c8c19e7f6c51f4638336d8f373a5d7eb6dd50
HowieBird <76828528@qq.com>
2017-10-09 11:42:51 +0800

    版本控制

---
+++ frontend/modules/app/actions/IndexAction.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 2017/10/9
+ * Time: 上午11:36
+ */
+
+namespace app\modules\app\actions;
+
+
+use yii\base\Action;
+
+class IndexAction extends Action
+{
+    public function run()
+    {
+        echo '12312';
+    }
+}
