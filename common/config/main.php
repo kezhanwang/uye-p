71da334c56b3b45c094b77be9347ef29ee2c846e
HowieBird <76828528@qq.com>
2017-10-11 14:45:45 +0800

    配置

--- common/config/main.php
+++ common/config/main.php
@@ -5,26 +5,12 @@ require(__DIR__ . DIRECTORY_SEPARATOR . "constant.php");
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'name' => 'U业',
-    'language'=>'zh-CN',
+    'language' => 'zh-CN',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
 
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=123.57.138.234;dbname=uye',
-            'username' => 'kz',
-            'password' => 'DB_root_#yulan#',
-            'charset' => 'utf8',
-        ],
-
-        'db2' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=123.57.138.234;dbname=kz',
-            'username' => 'kz',
-            'password' => 'DB_root_#yulan#',
-            'charset' => 'utf8',
-        ],
+        
     ],
 ];
