08da4e4c2066b4fac742bd9421ee73b23a276c22
HowieBird <76828528@qq.com>
2017-11-15 10:07:22 +0800

    E平台巨幕

--- e/views/site/index.php
+++ e/views/site/index.php
@@ -9,3 +9,8 @@ $this->title = '首页';
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['menu'] = $this->title;
 ?>
+
+<div class="jumbotron">
+    <h1>尊敬的用户您好：</h1>
+    <p>欢迎使用U业帮-E机构业务管理平台</p>
+</div>
