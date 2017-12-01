ded47b8d1355665a98d5cf5712660fd2bb468f33
HowieBird <76828528@qq.com>
2017-12-01 16:52:23 +0800

    1、机构E平台增加手机U业帮APP二维码

--- e/views/layouts/header.php
+++ e/views/layouts/header.php
@@ -2,7 +2,7 @@
 $userInfo = \e\components\RbacUtil::checkUserRight(Yii::$app->user->getId());
 ?>
 <style>
-    .searchform span {
+    .org_name span {
         box-shadow: none;
         float: left;
         font-size: 14px;
@@ -13,11 +13,25 @@ $userInfo = \e\components\RbacUtil::checkUserRight(Yii::$app->user->getId());
 </style>
 <div class="header-section">
     <a class="toggle-btn"><i class="fa fa-bars"></i></a>
-    <div class="searchform">
+    <div class="org_name">
         <span><?= $userInfo['org_name']; ?></span>
     </div>
     <div class="menu-right">
         <ul class="notification-menu">
+            <li>
+                <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-mobile-phone"></i>
+                    手机U业帮
+                    <span class="caret"></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-head pull-right" style="min-width: 129px;">
+                    <h5 class="title">下载U业帮客户端</h5>
+                    <a href="#">
+                        <img src="http://www.bjzhongteng.com/images/1510212394.png" style="width: 128px">
+                    </a>
+
+                </div>
+            </li>
             <li>
                 <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                     <img src="/images/photos/user-avatar.png" alt=""/>
